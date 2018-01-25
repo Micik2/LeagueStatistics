e88e9187e2a2d7f5102fd5c6f035b4cd1cd0519e
Micik2 <micik220@gmail.com>
2018-01-25 01:26:30 +0100

    Dalsza część

--- app/src/main/java/com/example/maciek/leaguestatistics/Match.java
+++ app/src/main/java/com/example/maciek/leaguestatistics/Match.java
@@ -1,38 +1,128 @@
 package com.example.maciek.leaguestatistics;
 
 
+import android.graphics.Bitmap;
 import android.icu.text.SimpleDateFormat;
-import android.icu.util.TimeZone;
 import android.icu.util.Calendar;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 
 import java.util.Date;
-import java.sql.Time;
 
 /**
  * Created by Maciek on 2018-01-11.
  */
 
 public class Match {
+    private int gameId;
+    //private String gameMode;
+    //private int queueId;
+    private String queueName;
+    //private int mapId;
+    private String mapName;
+    //private int gameDuration;
+    private String gameTime;
+
+    //private boolean win;
+    private String gameResult;
+    private int goldEarned;
+    private int totalMinionsKilled;
+
+    private String[] summonersNames = new String[10];
+    private String[] lanes = new String[10];
+    private int[] championIds = new int[10];
     private int[] kills = new int[10];
     private int[] deaths = new int[10];
     private int[] assists = new int[10];
-    private int goldEarned;
-    private int totalMinionsKilled;
-    private int gameDuration;
-    private String gameResult;
-    private String[] summonersNames = new String[10];
+    private String[] highestAchievedSeasonTiers = new String[10];
+    private Bitmap[] bitmaps = new Bitmap[10];
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public Match(int gI, int qI, /*String gM,*/ int mI, int gD, boolean gR, int gE, int tMK, String[] sN, String[] l, int[] cI, int[] k, int[] d, int[] a, String[] hAST, Bitmap[] b) {
+        this.gameId = gI;
+        //this.gameMode = gM;
+        this.queueName = getQueueName(qI);
+        this.mapName = getMapName(mI);
+        this.gameTime = getGameDuration(gD);
+        this.gameResult = getResult(gR, gD);
+        this.goldEarned = gE;
+        this.totalMinionsKilled = tMK;
+        this.summonersNames = sN;
+        this.lanes = l;
+        this.championIds = cI;
+        this.kills = k;
+        this.deaths = d;
+        this.assists = a;
+        this.highestAchievedSeasonTiers = hAST;
+        this.bitmaps = b;
+        //this.championId = cI;
+        /*switch (r) {
+            case "BOTTOM":
+                this.role = "BOT";
+        }*/
+        //this.role = r;
+        //this.date = getDate(t);
+        //if (gR < 300) then gameResult = "remake"
+    }
+
+    public String getQueueName(int qI) {
+        switch(qI) {
+            case 400:
+                return "Klasyczna draft";
+            case 420:
+                return "Rankingowa SOLO/DUO";
+            case 430:
+                return "Klasyczna w ciemno (5 vs 5)";
+            case 440:
+                return "Rankingowa FLEX (5 vs 5)";
+            case 450:
+                return "ARAM";
+            case 460:
+                return "Klasyczna w ciemno (3 vs 3)";
+            case 470:
+                return "Rankingowa FLEX (3 vs 3)";
+            default:
+                return "Niestandardowa";
+        }
+    }
+
+    public String getMapName(int mI) {
+        switch(mI) {
+            case 3:
+                return "The Proving Grounds";
+            case 10:
+                return "Twisted Treeline";
+            case 11:
+                return "Summoner's Rift";
+            case 12:
+                return "Howling Abyss";
+            default:
+                return "Nieznana mapa";
+        }
+    }
+
+    public String getGameDuration(int gD) {
+        int minutes = gD / 60;
+        int seconds = gD % 60;
+        return String.format("%dmin %ds", minutes, seconds);
+    }
+
+    public String getResult(boolean gR, int gD) {
+        if (gD < 300)
+            return "REMAKE";
+        else if (gR)
+            return "WYGRANA";
+        return "PRZEGRANA";
+    }
     //private String gameType;
     //private Time gameDuration;
-    private int championId;
-    private String lane;
+
     //private long timestamp;
     //private String date;
     //private champpionImage;
-    private String gameMode;
-    private int mapId;
-    private String highestAchievedSeasonTier;
+
+
+
     //private int spell1Id;
     //private int spell2Id;
     //private int kills;
@@ -53,18 +143,8 @@ public class Match {
     }
     */
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public Match(int cI, String r, long t) {
-        this.championId = cI;
-        /*switch (r) {
-            case "BOTTOM":
-                this.role = "BOT";
-        }*/
-        this.role = r;
-        this.date = getDate(t);
-        //if (gR < 300) then gameResult = "remake"
-    }
 
+    /*
     @RequiresApi(api = Build.VERSION_CODES.N)
     public String getDate(long t) {
         Calendar calendar = Calendar.getInstance();
@@ -78,5 +158,5 @@ public class Match {
 
         return championName;
     }
-
+    */
 }
--- app/src/main/java/com/example/maciek/leaguestatistics/SearchSummoner.java
+++ app/src/main/java/com/example/maciek/leaguestatistics/SearchSummoner.java
@@ -3,6 +3,8 @@ package com.example.maciek.leaguestatistics;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -27,10 +29,6 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-<<<<<<< HEAD
-=======
-import java.util.List;
->>>>>>> d290120363ec1a505046aaa063921d7cb31fb996
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -43,14 +41,26 @@ public class SearchSummoner extends AppCompatActivity {
     private EditText searchText;
     private TextView accountIdTest;
     private ListView listView;
-<<<<<<< HEAD
     private int id;
-    private int status = 0;
+    private int status = -1;
     private int amountMatches = 0;
-=======
     private ArrayList<Match> matches = new ArrayList<Match>();
     private MyAdapter myAdapter;
->>>>>>> d290120363ec1a505046aaa063921d7cb31fb996
+    private String[] summonersNames = new String[10];
+    private String summonerName;
+    private int[] kills = new int[10];
+    private int[] deaths = new int[10];
+    private int[] assists = new int[10];
+    private String[] highestAchievedSeasonTiers = new String[10];
+    private String[] lanes = new String[10];
+    //private String lane;
+    private int mapId;
+    //private int[] gameId = new int[10];
+    private int gameId;
+    private int[] championIds = new int[10];
+    private String[] imagesNames = new String[10];
+    private String fullImageUrl;
+    private Bitmap[] bitmaps = new Bitmap[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +75,7 @@ public class SearchSummoner extends AppCompatActivity {
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            String summonerName = searchText.getText().toString();
+            summonerName = searchText.getText().toString();
             new API().execute(new String[]{summonerName});
             //accountIdTest.setText(api.getAccountId(summonerName));
             }
@@ -74,29 +84,27 @@ public class SearchSummoner extends AppCompatActivity {
 
     private class API extends AsyncTask<String, Void, String> {
         private final int accountId = 31084320;
-<<<<<<< HEAD
         private final String APIkey = "RGAPI-a7328b83-555c-47e9-9a69-0a87c5804064";
-=======
-        private final String APIkey = "RGAPI-24434203-a0b0-44ab-850c-9b1d90305e4c";
->>>>>>> d290120363ec1a505046aaa063921d7cb31fb996
         private String accountUrl = "https://eun1.api.riotgames.com/lol/summoner/v3/summoners/by-name/%s?api_key=" + APIkey;
         private String matchesUrl = "https://eun1.api.riotgames.com/lol/match/v3/matchlists/by-account/%d/recent?api_key=" + APIkey;
         private String matchUrl = "https://eun1.api.riotgames.com/lol/match/v3/matches/%d?api_key=" + APIkey;
+        private String championsUrl = "https://eun1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&tags=image&dataById=true&api_key=" + APIkey;
+        private String ddragonImageUrl = "https://ddragon.leagueoflegends.com/cdn/8.2.1/img/champion/";
         private ProgressDialog progressDialog = new ProgressDialog(SearchSummoner.this);
         private ArrayList<Match> matches = new ArrayList<Match>();
 
-        public String getAccountUrl(String summonerName) {
-            String fullAccountUrl = String.format(accountUrl, summonerName);
+        public String getAccountUrl(String sN) {
+            String fullAccountUrl = String.format(accountUrl, sN);
             return fullAccountUrl;
         }
 
-        public String getMatchesUrl(int accountId) {
-            String fullMatchesUrl = String.format(matchesUrl, accountId);
+        public String getMatchesUrl(int aI) {
+            String fullMatchesUrl = String.format(matchesUrl, aI);
             return fullMatchesUrl;
         }
 
-        public String getMatchUrl(long gameId) {
-            String fullMatchUrl = String.format(matchUrl, gameId);
+        public String getMatchUrl(long gI) {
+            String fullMatchUrl = String.format(matchUrl, gI);
             return fullMatchUrl;
         }
 
@@ -137,11 +145,13 @@ public class SearchSummoner extends AppCompatActivity {
             URL url = null;
             try {
                 if (status == 2) {
-                    if (amountMatches < 10) {
+                    if (amountMatches < 11) {
                         url = new URL(getMatchUrl(Long.parseLong(params[0])));
-                        amountMatches += 1;
-                        status = 3;
+                        //amountMatches += 1;
+                        //status = 3;
                     }
+                    else
+                        status = 4;
                 }
                 else if (status == 1) {
                     url = new URL(getMatchesUrl(Integer.parseInt(params[0])));
@@ -150,6 +160,10 @@ public class SearchSummoner extends AppCompatActivity {
                 else if (status == 0) {
                     url = new URL(getAccountUrl(params[0]));
                     status = 1;
+                }
+                else if (status == -1) {
+                    url = new URL(championsUrl);
+                    status = 0;
                 }
                 //url = new URL(getMatchesUrl(accountId));
             } catch (MalformedURLException e) {
@@ -192,43 +206,98 @@ public class SearchSummoner extends AppCompatActivity {
             accountIdTest = (TextView) findViewById(R.id.accountIdTest);
             accountIdTest.setText(Integer.toString(accountId));
             */
-            int gameId = 0;
+            //int gameId = 0;
             try {
                 JSONObject jsonObject = new JSONObject(result);
-<<<<<<< HEAD
-                if (status == 3) {
 
+                if (status == 3) {
+                    JSONArray jsonArray = jsonObject.getJSONArray("participantIdentities");
+                    for (int j = 0; j < 10; j++) {
+                        JSONObject participantId = jsonArray.getJSONObject(j);
+                        JSONObject player = participantId.getJSONObject("player");
+                        summonersNames[j] = player.getString("summonerName");
+                        // numer porządkowy osoby (od 1 do 10)
+                        int partId = -1;
+                        if (summonersNames[j].equals(summonerName))
+                            partId = participantId.getInt("participantId");
+                        JSONArray participants = jsonObject.getJSONArray("participants");
+                        //int kills = -1;
+                        //int deaths = -1;
+                        //int assists = -1;
+                        boolean win;
+                        int goldEarned = -1;
+                        int totalMinionsKilled = -1;
+                        int gameDuration = -1;
+                        //String highestAchievedSeasonTier = "";
+                        for (int k = 0; k < 10; k++) {
+                            JSONObject object = participants.getJSONObject(k);
+                            JSONObject stats = object.getJSONObject("stats");
+                            kills[k] = stats.getInt("kills");
+                            deaths[k] = stats.getInt("deaths");
+                            assists[k] = stats.getInt("assists");
+                            int pI = object.getInt("participantId");
+                            highestAchievedSeasonTiers[k] = object.getString("highestAchievedSeasonTier");
+                            championIds[k] = object.getInt("championId");
+                            JSONObject timeline = object.getJSONObject("timeline");
+                            lanes[k] = timeline.getString("lane");
+                            if (partId == pI) {
+                                win = stats.getBoolean("win");
+                                goldEarned = stats.getInt("goldEarned");
+                                totalMinionsKilled = stats.getInt("totalMinionsKilled");
+                            }
+                        }
+                        // CLASSIC lub RANKED
+                        String gameMode = jsonObject.getString("gameMode");
+                        // id mapy
+                        mapId = jsonObject.getInt("mapId");
+                        gameDuration = jsonObject.getInt("gameDuration");
+                    }
                 }
                 //JSONObject jsonObject = new JSONObject(result);
                 else if (status == 2) {
                     JSONArray jsonArray = jsonObject.getJSONArray("matches");
                     for (int i = 0; i < 10; i++) {
                         JSONObject singleMatch = jsonArray.getJSONObject(i);
-                        String lane = singleMatch.getString("lane");
                         gameId = singleMatch.getInt("gameId");
-                        int champion = singleMatch.getInt("champion");
                     }
 
                 }
                 else if (status == 1) {
                     id = jsonObject.getInt("accountId");
                 }
+                else if (status == 0) {
+                    JSONObject datas = jsonObject.getJSONObject("data");
+                    for (int n = 0; n < 10; n++) {
+                        JSONObject data = datas.getJSONObject(String.valueOf(championIds[n]));
+                        JSONObject images = data.getJSONObject("image");
+                        imagesNames[n] = images.getString("full");
+                        URL fullImageUrl = new URL(ddragonImageUrl + imagesNames[n]);
+                        bitmaps[n] = BitmapFactory.decodeStream(fullImageUrl.openConnection().getInputStream());
+                    }
+
+                }
 
 
-=======
-                JSONArray jsonArray = jsonObject.getJSONArray("matches");
-                JSONObject singleMatchJSON = jsonArray.getJSONObject(0);
-                Match singleMatch = new Match(singleMatchJSON.getInt("champion"), singleMatchJSON.getString("role"), singleMatchJSON.getLong("date"));
-                matches.add(singleMatch);
-                myAdapter = new MyAdapter(getApplicationContext(), matches);
-                listView = (ListView) findViewById(R.id.listview);
-                listView.setAdapter(myAdapter);
->>>>>>> d290120363ec1a505046aaa063921d7cb31fb996
+                //JSONArray jsonArray = jsonObject.getJSONArray("matches");
+                //JSONObject singleMatchJSON = jsonArray.getJSONObject(0);
+                //Match singleMatch = new Match(singleMatchJSON.getInt("champion"), singleMatchJSON.getString("role"), singleMatchJSON.getLong("date"));
+                //matches.add(singleMatch);
+                //myAdapter = new MyAdapter(getApplicationContext(), matches);
+                //listView = (ListView) findViewById(R.id.listview);
+                //listView.setAdapter(myAdapter);
             } catch (JSONException e) {
                 e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            if (status == 3)
+            if (status == 3) {
+                for (int m = 0; m < 10; m++) {
+                    matches.add(new Match())
+                }
                 progressDialog.dismiss();
+            }
             else if (status == 2) {
                 new API().execute(new String[]{Integer.toString(gameId)});
                 //if (amountMatches == 10)
